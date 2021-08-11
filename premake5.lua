d9bcd4a76dcc9b73e02a05f62324c774f568cdd1
shreyaspranav <shrepranav5@gmail.com>
2021-08-11 16:17:37 +0530

    updated gitignore

--- premake5.lua
+++
@@ -1,8 +0,0 @@
-workspace "Vertex"
-    architecture "x64"
-    configurations {"Debug", "Release"}
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
-include "Vertex/"
-include "SandboxApp/"
