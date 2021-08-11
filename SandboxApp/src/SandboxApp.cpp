9753613ab5591634a3c586d473f5b3d19666df44
shreyaspranav <shrepranav5@gmail.com>
2021-08-11 16:31:07 +0530

    Update SandboxApp.cpp

--- SandboxApp/src/SandboxApp.cpp
+++ SandboxApp/src/SandboxApp.cpp
@@ -1,12 +1,17 @@
 #include <Vertex.h>
+#include <iostream>
 
 class SandboxApp : public Vertex::Application
 {
 public:
 	SandboxApp()
-	{}
+	{
+		std::cout << "Hello Sandbox!\n";
+	}
 	~SandboxApp()
-	{}
+	{
+		std::cout << "Bye Sandbox!\n";
+	}
 };
 
 int main()
